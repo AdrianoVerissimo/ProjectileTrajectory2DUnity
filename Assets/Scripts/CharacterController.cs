ead709c2a7bb0aa52cda243a8ef521ba75716389
Adriano Veríssimo <adrverissimo@yahoo.com.br>
2018-03-26 20:18:34 -0300

    Criando script para personagem; criando script para o projétil.

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            print("pew pew");
+        }
+
+	}
+
+    public void DrawTrajectory(Vector2 startPos, Vector2 startVelocity)
+    {
+        int verts = 20;
+        LineRenderer line = GetComponent<LineRenderer>();
+        line.SetVertexCount(verts);
+ 
+        Vector2 pos = startPos;
+        Vector2 vel = startVelocity;
+        Vector2 grav = new Vector2(Physics2D.gravity.x, Physics2D.gravity.y);
+        for(var i = 0; i<verts; i++)
+        {
+            line.SetPosition(i, new Vector3(pos.x, pos.y, 0f));
+            vel = vel + grav* Time.fixedDeltaTime;
+        pos = pos + vel* Time.fixedDeltaTime;
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileController : MonoBehaviour {
+
+    public Vector2 velocity;
+    public float bounce_damping = 1f;
+    public LayerMask layerHit;
+
+    private Vector2 gravity;
+    private Vector2 last_pos;
+
+	// Use this for initialization
+	void Start () {
+        gravity = Physics2D.gravity;
+        last_pos = Vector2.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float play_speed = Time.deltaTime;
+        velocity += gravity * play_speed;
+        RaycastHit2D hit;
+        last_pos = transform.position;
+        hit = Physics2D.Linecast(last_pos, (last_pos + (velocity * play_speed)), layerHit);
+        if (hit)
+        {
+            velocity = Vector2.Reflect(velocity * bounce_damping, hit.normal);
+            transform.position = hit.point;
+        }
+        transform.position += (Vector3)(velocity * play_speed);
+    }
+}
